d48fc84d6dbf7883d95b5f00acafce5b0890fcf5
LucMLC <lucas.docanto08@gmail.com>
2023-12-07 16:57:01 -0300

    Criação do layout resultado do sorteio

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function generationNumber(){
     
-
+    const paragrafo = document.querySelector('p')
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
    
     const result = Math.floor(Math.random() * (max - min + 1) + min); 
 
-    alert(result)
+    paragrafo.innerHTML = result
+    paragrafo.style.backgroundColor = "#AEC346"
+
+
 }
